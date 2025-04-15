8e40ef6372c6259525aff935a70dd273a6c718ce
cpbergen <cpbergen@umich.edu>
2025-04-15 15:38:51 -0400

    Add files via upload

--- js/reviewPage.js
+++ js/reviewPage.js
@@ -1,27 +1,207 @@
 // Code provided by Blair Bocklet
-function showOverlay() {
-document.querySelector(".overlay").style.display = "block"; }
+// function showOverlay() {
+// document.querySelectorAll(".overlay").style.display = "block"; }
 
-function hideOverlay() {
-    document.querySelector(".overlay").style.display = "none";}
+// function hideOverlay() {
+// document.querySelector(".overlay").style.display = "none";}
+   
+let selectOverlay = document.querySelectorAll(".overlay")
 
 
 //This code is mine
-document.querySelector(".movie-cell").addEventListener("click", function() {
+//Code for Movie 1 in the page list.
+document.querySelectorAll(".movie-cell")[0].addEventListener("click", function() {
     console.log("clicked");
-// if (document.getElementById("overlay").style.display == "none") {
-//     showOverlay();
-// } else
-// { hideOverlay();}
+
+if (document.querySelectorAll(".overlay")[0].style.display == "none") {
+    selectOverlay[0].style.display="block";
+} else
+{ selectOverlay[0].style.display="none"}
 } )
 
-document.querySelector(".review-cell").addEventListener("click", function(){
-if (document.querySelector(".overlay").style.display != "none") {
-    hideOverlay();
+document.querySelectorAll(".review-cell")[0].addEventListener("click", function(){
+if (document.querySelectorAll(".overlay")[0].style.display != "none") {
+    selectOverlay[0].style.display="none";
 }
 else {console.log("something went wrong");}
 }
 )
+
+//Code for Movie 2 in the page list.
+document.querySelectorAll(".movie-cell")[1].addEventListener("click", function() {
+    console.log("clicked1");
+if (document.querySelectorAll(".overlay")[1].style.display == "none") {
+    selectOverlay[1].style.display="block";
+} else
+{ selectOverlay[1].style.display="none"}
+} )
+
+document.querySelectorAll(".review-cell")[1].addEventListener("click", function(){
+if (document.querySelectorAll(".overlay")[1].style.display != "none") {
+   selectOverlay[1].style.display="none"
+}
+else {console.log("something went wrong");}
+}
+)
+
+//Code for Movie 3 in the page list.
+document.querySelectorAll(".movie-cell")[2].addEventListener("click", function() {
+    console.log("clicked2");
+if (document.querySelectorAll(".overlay")[2].style.display == "none") {
+    selectOverlay[2].style.display="block";
+} else
+{ selectOverlay[2].style.display="none"}
+} )
+
+document.querySelectorAll(".review-cell")[2].addEventListener("click", function(){
+if (document.querySelectorAll(".overlay")[2].style.display != "none") {
+   selectOverlay[2].style.display="none"
+}
+else {console.log("something went wrong");}
+}
+)
+
+//Code for Movie 4 in the page list.
+document.querySelectorAll(".movie-cell")[3].addEventListener("click", function() {
+    console.log("clicked3");
+if (document.querySelectorAll(".overlay")[3].style.display == "none") {
+    selectOverlay[3].style.display="block";
+} else
+{ selectOverlay[3].style.display="none"}
+} )
+
+document.querySelectorAll(".review-cell")[3].addEventListener("click", function(){
+if (document.querySelectorAll(".overlay")[3].style.display != "none") {
+   selectOverlay[3].style.display="none"
+}
+else {console.log("something went wrong");}
+} )
+
+//Code for Movie 5 in the page list.
+document.querySelectorAll(".movie-cell")[4].addEventListener("click", function() {
+    console.log("clicked4");
+if (document.querySelectorAll(".overlay")[4].style.display == "none") {
+    selectOverlay[4].style.display="block";
+} else
+{ selectOverlay[4].style.display="none"}
+} )
+
+document.querySelectorAll(".review-cell")[4].addEventListener("click", function(){
+if (document.querySelectorAll(".overlay")[4].style.display != "none") {
+   selectOverlay[4].style.display="none"
+}
+else {console.log("something went wrong");}
+} )
+
+
+//Code for Movie 6 in the page list.
+document.querySelectorAll(".movie-cell")[5].addEventListener("click", function() {
+    console.log("clicked5");
+if (document.querySelectorAll(".overlay")[5].style.display == "none") {
+    selectOverlay[5].style.display="block";
+} else
+{ selectOverlay[5].style.display="none"}
+} )
+
+document.querySelectorAll(".review-cell")[5].addEventListener("click", function(){
+if (document.querySelectorAll(".overlay")[5].style.display != "none") {
+selectOverlay[5].style.display="none"
+}
+else {console.log("something went wrong");}
+} )
+
+
+//Code for Movie 7 in the page list.
+document.querySelectorAll(".movie-cell")[6].addEventListener("click", function() {
+    console.log("clicked6");
+if (document.querySelectorAll(".overlay")[6].style.display == "none") {
+    selectOverlay[6].style.display="block";
+} else
+{ selectOverlay[6].style.display="none"}
+} )
+
+document.querySelectorAll(".review-cell")[6].addEventListener("click", function(){
+if (document.querySelectorAll(".overlay")[6].style.display != "none") {
+selectOverlay[6].style.display="none"
+}
+else {console.log("something went wrong");}
+} )
+
+//Code for Movie 8 in the page list.
+document.querySelectorAll(".movie-cell")[7].addEventListener("click", function() {
+    console.log("clicked7");
+if (document.querySelectorAll(".overlay")[7].style.display == "none") {
+    selectOverlay[7].style.display="block";
+} else
+{ selectOverlay[7].style.display="none"}
+} )
+
+document.querySelectorAll(".review-cell")[7].addEventListener("click", function(){
+if (document.querySelectorAll(".overlay")[7].style.display != "none") {
+selectOverlay[7].style.display="none"
+}
+else {console.log("something went wrong");}
+} )
+
+//Code for Movie 9 in the page list.
+document.querySelectorAll(".movie-cell")[8].addEventListener("click", function() {
+    console.log("clicked8");
+if (document.querySelectorAll(".overlay")[8].style.display == "none") {
+    selectOverlay[8].style.display="block";
+} else
+{ selectOverlay[8].style.display="none"}
+} )
+
+document.querySelectorAll(".review-cell")[8].addEventListener("click", function(){
+if (document.querySelectorAll(".overlay")[8].style.display != "none") {
+selectOverlay[8].style.display="none"
+}
+else {console.log("something went wrong");}
+} )
+
+//Code for Movie 10 in the page list.
+document.querySelectorAll(".movie-cell")[9].addEventListener("click", function() {
+    console.log("clicked9");
+if (document.querySelectorAll(".overlay")[9].style.display == "none") {
+    selectOverlay[9].style.display="block";
+} else
+{ selectOverlay[9].style.display="none"}
+} )
+
+document.querySelectorAll(".review-cell")[9].addEventListener("click", function(){
+if (document.querySelectorAll(".overlay")[9].style.display != "none") {
+selectOverlay[9].style.display="none"
+}
+else {console.log("something went wrong");}
+} )
+
+//Code for Movie 11 in the page list.
+document.querySelectorAll(".movie-cell")[10].addEventListener("click", function() {
+    console.log("clicked10");
+if (document.querySelectorAll(".overlay")[10].style.display == "none") {
+    selectOverlay[10].style.display="block";
+} else
+{ selectOverlay[10].style.display="none"}
+} )
+
+document.querySelectorAll(".review-cell")[10].addEventListener("click", function(){
+if (document.querySelectorAll(".overlay")[10].style.display != "none") {
+selectOverlay[10].style.display="none"
+}
+else {console.log("something went wrong");}
+} )
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // function showOverlay() {
@@ -34,7 +214,7 @@ else {console.log("something went wrong");}
     // }
 // // Example: Show overlay after 2 seconds
 
-setTimeout(showOverlay, 2000);
+// setTimeout(showOverlay, 2000);
 
 // // Example: Hide overlay on click
 // document.getElementById("overlay").addEventListener("click", hideOverlay);
